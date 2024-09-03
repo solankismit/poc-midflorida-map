360ac27d60361496e9f58b7da415657c50ee59b5
solankismit <114059076+solankismit@users.noreply.github.com>
2024-09-03 16:22:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,9 +28,7 @@ def calculate_distance(lat1, lon1, lat2, lon2):
   R = 6371  # Radius of the Earth in kilometers
   dLat = deg2rad(lat2 - lat1)
   dLon = deg2rad(lon2 - lon1)
-  a = math.sin(dLat / 2) * math.sin(dLat / 2) + \
-      math.cos(deg2rad(lat1)) * math.cos(deg2rad(lat2)) * \
-      math.sin(dLon / 2) * math.sin(dLon / 2)
+  a = math.sin(dLat / 2) * math.sin(dLat / 2) + math.cos(deg2rad(lat1)) * math.cos(deg2rad(lat2)) * math.sin(dLon / 2) * math.sin(dLon / 2)
   c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
   distance = R * c  # Distance in kilometers
   distance_in_miles = math.floor(distance / 1.609) 
